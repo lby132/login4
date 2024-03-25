8e351df910e965fdcf86b78704f1f6d3be1f1e75
lby132 <quddbscjswow@naver.com>
2024-03-25 23:51:39 +0900

    Initial commit

---
+++ src/main/java/hello/login/domain/login/LoginService.java
@@ -0,0 +1,21 @@
+package hello.login.domain.login;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    public Member login(String loginId, String password) {
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+}
