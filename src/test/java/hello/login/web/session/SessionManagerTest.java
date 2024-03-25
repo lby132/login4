ac1e06d524cfb639addf12859e38ed4e0ea97bb2
lby132 <quddbscjswow@naver.com>
2024-03-26 00:01:19 +0900

    session

---
+++ src/test/java/hello/login/web/session/SessionManagerTest.java
@@ -0,0 +1,34 @@
+package hello.login.web.session;
+
+import hello.login.domain.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionManagerTest {
+
+    SessionManager sessionManager = new SessionManager();
+
+    @Test
+    void test() {
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        final Member member = new Member();
+        sessionManager.createSession(member, response);
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setCookies(response.getCookies());
+
+        Object result = sessionManager.getSession(request);
+        assertThat(result).isEqualTo(member);
+
+        sessionManager.expire(request);
+        final Object expired = sessionManager.getSession(request);
+        assertThat(expired).isNull();
+    }
+}
